431ad75db848cd13e15b2b3607d45469d320e3c8
Conrelia Harder <cornelialisaharder@gmail.com>
2022-09-20 18:37:35 +0200

    maincontent och animation bilder

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
+window.addEventListener("load", animationImage);
 
+let image1 = document.getElementById("image1");
+let image2 = document.getElementById("image2");
+
+function animationImage() {
+  image1.style.display = "block";
+  image1.className = "imgFrontPage";
+  setTimeout(() => {
+    image2.style.display = "block";
+    image2.className = "imgFrontPage";
+  }, 2000);
+}
